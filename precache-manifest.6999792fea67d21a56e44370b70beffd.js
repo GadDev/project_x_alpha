620a0ee9affe83007b4b4d7c6cfcd88da625270c
Alex <alex.gadaix@gmail.com>
2019-10-26 23:31:50 +0100

    Updates

---
+++ precache-manifest.6999792fea67d21a56e44370b70beffd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf0b2db52757af98ee61038ef9396b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4bda166329e96547531",
+    "url": "/static/css/main.86674063.chunk.css"
+  },
+  {
+    "revision": "8455a9f948a9152c92fb",
+    "url": "/static/js/2.4bbc3513.chunk.js"
+  },
+  {
+    "revision": "e4bda166329e96547531",
+    "url": "/static/js/main.bb0e3b75.chunk.js"
+  },
+  {
+    "revision": "90a2f2642b8e75db0aaf",
+    "url": "/static/js/runtime-main.dfa45b16.js"
+  }
+]);
